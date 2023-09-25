ff5b88454bd69f2431884d6e2332bca2886263cf
Felipe Carvalho <felipecarvalho180@gmail.com>
2023-09-25 20:19:19 -0300

    feat: add combobox validation

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -16,62 +16,90 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Option } from "@/utils/types";
-import { UseFormRegister, UseFormSetValue } from "react-hook-form";
+import {
+  Control,
+  UseFormRegister,
+  UseFormSetValue,
+  UseFormTrigger,
+} from "react-hook-form";
+import { FormField, FormItem, FormMessage } from "@/components/ui/form";
 
 interface Props {
   options: Option[];
   register: UseFormRegister<any>;
   name: string;
   setValue: UseFormSetValue<any>;
-  value: string;
+  error?: string;
+  control: Control<any>;
+  trigger: UseFormTrigger<any>;
 }
 
-export function Combobox({ options, register, name, setValue, value }: Props) {
+export function Combobox({
+  options,
+  register,
+  name,
+  setValue,
+  control,
+  trigger,
+}: Props) {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between text-zinc-900"
-        >
-          {value
-            ? options.find((option) => option.value === value)?.label
-            : "Selecione uma opção"}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[var(--radix-popper-anchor-width)] p-0 ">
-        <Command id={name}>
-          <CommandInput placeholder="Digite uma moeda..." />
-          <CommandEmpty>Nenhuma moeda encontrada.</CommandEmpty>
-          <CommandGroup>
-            {options.map((option) => (
-              <CommandItem
-                key={option.value}
-                id={name}
-                value={option.label}
-                onSelect={() => {
-                  setValue(name, option.value);
-                  setOpen(false);
-                }}
-                {...register(name)}
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-full justify-between text-zinc-900"
               >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === option.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {option.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                {field.value
+                  ? options.find((option) => option.value === field.value)
+                      ?.label
+                  : "Selecione uma opção"}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[var(--radix-popper-anchor-width)] p-0 ">
+              <Command id={name}>
+                <CommandInput placeholder="Digite uma moeda..." />
+                <CommandEmpty>Nenhuma moeda encontrada.</CommandEmpty>
+                <CommandGroup>
+                  {options.map((option) => (
+                    <CommandItem
+                      key={option.value}
+                      id={name}
+                      value={option.label}
+                      onSelect={async () => {
+                        setValue(name, option.value);
+                        setOpen(false);
+                        await trigger(name);
+                      }}
+                      {...register(name)}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          field.value === option.value
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {option.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
+          </Popover>
+          <FormMessage className="text-xs -mt-5 ml-3 text-red-500" />
+        </FormItem>
+      )}
+    />
   );
 }
--- src/components/ui/datePicker.tsx
+++ src/components/ui/datePicker.tsx
@@ -16,34 +16,40 @@ interface Props {
   name: string;
   setValue: UseFormSetValue<any>;
   value: Date;
+  error?: string;
 }
 
-export function DatePicker({ name, register, setValue, value }: Props) {
+export function DatePicker({ name, register, setValue, value, error }: Props) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-full justify-start text-left font-normal",
-            !value ? "text-muted-foreground" : "text-zinc-700"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {value ? format(value, "PPP") : <span>Selecione uma data</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={value}
-          onSelect={(newValue) => {
-            setValue(name, newValue);
-          }}
-          {...register(name)}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-full justify-start text-left font-normal",
+              !value ? "text-muted-foreground" : "text-zinc-700"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {value ? format(value, "PPP") : <span>Selecione uma data</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={value}
+            onSelect={(newValue) => {
+              setValue(name, newValue);
+            }}
+            {...register(name)}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+      {error && (
+        <span className="text-xs -mt-3 ml-3 text-red-500">{error}</span>
+      )}
+    </>
   );
 }
